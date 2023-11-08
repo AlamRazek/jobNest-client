b3c94fd11be1394327b90183724b9d4c4f1aaf34
AlamRazek <ohidul933gmail.com>
2023-11-08 22:03:30 +0000

    navbar added with private route

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Banner from "./Banner";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <div className="h-[100vh]">
+      <Helmet>
+        <title>Home </title>
+      </Helmet>
       <Banner></Banner>
       <h2>This is home</h2>
     </div>
